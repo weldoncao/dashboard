c58d20e1e8b159e466779df0277720d1e847466c
Wei Cao <wei.cao@turn.com>
2017-05-23 15:56:33 -0700

    Layout half way

--- src/App.js
+++ src/App.js
@@ -2,15 +2,75 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import SimpleBarChart from './BarChart';
+import HackBarChart from './HackBarChart';
+import HackPieChart from './HackPieChart';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-intro">
-          <SimpleBarChart />
-          <SimpleBarChart />
+      <div className="container-fluid">
+        <h4 className="page-header">Market Name</h4>
+        <div className="card hack-card-left">
+          <div className="card-block">
+            <div className="card-title">User Activities</div>
+            <p className="card-text">1.26M</p>
+          </div>
+        </div>
+        <div className="card hack-card-right">
+          <div className="card-block">
+            <div className="card-title">Uniques</div>
+            <p className="card-text">554.14K</p>
+          </div>
+        </div>
+        <div className="row placeholders hack-row">
+          <div className="col-xs-6 placeholder hack-col">
+            <h5 style={{marginLeft: 250}}>Demographics</h5>
+            <HackBarChart />
+          </div>
+          <div className="col-xs-6 placeholder hack-col">
+            <h5 style={{marginLeft: 250}}>Top Segments</h5>
+            <HackBarChart />
+          </div>
+        </div>
+        <div className="row placeholders hack-row">
+          <h5 style={{marginLeft: 600, marginTop: 20}}>Technographics</h5>
+          <div className="col-xs-6 placeholder hack-col">
+            <h5 style={{marginLeft: 230}}>Browser Share</h5>
+            <HackPieChart />
+          </div>
+          <div className="col-xs-6 placeholder hack-col">
+            <h5 style={{marginLeft: 290}}>Devices</h5>
+            <div>
+              <div className="hack-device">
+                <div><i className="fa fa-desktop" aria-hidden="true"></i></div>
+                <div className="hack-device-ratio">85%</div>
+              </div>
+              <div className="hack-device">
+                <div><i className="fa fa-mobile" aria-hidden="true"></i></div>
+                <div className="hack-device-ratio">10%</div>
+              </div>
+              <div className="hack-device"><div style={{fontSize: 20, paddingTop: 12, paddingBottom: 13}}>Other</div><div className="hack-device-ratio">5%</div></div>
+            </div>
+          </div>
+        </div>
+        <div className="row placeholders hack-row">
+          <h5 style={{marginLeft: 600, marginTop: 20}}>Geographics</h5>
+          <div className="col-xs-6 placeholder hack-col">
+            <HackPieChart />
+          </div>
+          <div className="col-xs-6 placeholder hack-col">
+            <div>
+              <div className="hack-device">
+                <div><i className="fa fa-desktop" aria-hidden="true"></i></div>
+                <div className="hack-device-ratio">85%</div>
+              </div>
+              <div className="hack-device">
+                <div><i className="fa fa-mobile" aria-hidden="true"></i></div>
+                <div className="hack-device-ratio">10%</div>
+              </div>
+              <div className="hack-device"><div style={{fontSize: 20, paddingTop: 12, paddingBottom: 13}}>Other</div><div className="hack-device-ratio">5%</div></div>
+            </div>
+          </div>
         </div>
       </div>
     );
