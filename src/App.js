732d32e3ac2dab0656052196077d006f3dd5119b
Wei Cao <wei.cao@turn.com>
2017-05-23 16:52:20 -0700

    More charts

--- src/App.js
+++ src/App.js
@@ -18,17 +18,67 @@ class App extends Component {
         </div>
         <div className="card hack-card-right">
           <div className="card-block">
-            <div className="card-title">Uniques</div>
+            <div className="card-title">Visits Per Hour</div>
             <p className="card-text">554.14K</p>
           </div>
         </div>
         <div className="row placeholders hack-row">
+        <h5 style={{marginLeft: 600, marginTop: 20}}>Demographics</h5>
           <div className="col-xs-6 placeholder hack-col">
-            <h5 style={{marginLeft: 250}}>Demographics</h5>
+            <h5 style={{marginLeft: 250}}>Age</h5>
             <HackBarChart />
           </div>
           <div className="col-xs-6 placeholder hack-col">
-            <h5 style={{marginLeft: 250}}>Top Segments</h5>
+            <h5 style={{marginLeft: 250}}>Occupation</h5>
+            <HackBarChart />
+          </div>
+          <div className="col-xs-6 placeholder hack-col">
+            <h5 style={{marginLeft: 250}}>Gender</h5>
+            <i className="fa fa-male" aria-hidden="true"></i>
+            <div className="progress">
+              <div className="progress-bar bg-success" role="progressbar" style={{width: "50%"}} aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <i className="fa fa-female" aria-hidden="true"></i>
+            <h5 style={{marginLeft: 250}}>Networth</h5>
+            <div>
+              <table className="table">
+                <tr>
+                  <td style={{textAlign: "center"}}>
+                  0-1000
+                  </td>
+                  <td>
+                  20%
+                  </td>
+                </tr>
+                <tr>
+                  <td style={{textAlign: "center"}}>
+                  0-1000
+                  </td>
+                  <td>
+                  20%
+                  </td>
+                </tr>
+                <tr>
+                  <td style={{textAlign: "center"}}>
+                  0-1000
+                  </td>
+                  <td>
+                  20%
+                  </td>
+                </tr>
+                <tr>
+                  <td style={{textAlign: "center"}}>
+                  0-1000
+                  </td>
+                  <td>
+                  20%
+                  </td>
+                </tr>
+              </table>
+            </div>
+          </div>
+          <div className="col-xs-6 placeholder hack-col">
+            <h5 style={{marginLeft: 250}}>Generation</h5>
             <HackBarChart />
           </div>
         </div>
@@ -70,6 +120,17 @@ class App extends Component {
               </div>
               <div className="hack-device"><div style={{fontSize: 20, paddingTop: 12, paddingBottom: 13}}>Other</div><div className="hack-device-ratio">5%</div></div>
             </div>
+          </div>
+        </div>
+        <div className="row placeholders hack-row">
+          <h5 style={{marginLeft: 600, marginTop: 20}}>Magic</h5>
+          <div className="col-xs-6 placeholder hack-col">
+            <h5 style={{marginLeft: 230}}>Top Segments</h5>
+            <HackBarChart />
+          </div>
+          <div className="col-xs-6 placeholder hack-col">
+            <h5 style={{marginLeft: 250}}>Realtime Classification</h5>
+            <HackBarChart />
           </div>
         </div>
       </div>
--- src/HackBarChart.js
+++ src/HackBarChart.js
@@ -3,13 +3,13 @@ import React, { Component } from 'react';
 import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
 
 const data = [
-      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+      {name: 'Page A', uv: 4000, amt: 2400},
+      {name: 'Page B', uv: 3000, amt: 2210},
+      {name: 'Page C', uv: 2000, amt: 2290},
+      {name: 'Page D', uv: 2780, amt: 2000},
+      {name: 'Page E', uv: 1890, amt: 2181},
+      {name: 'Page F', uv: 2390, amt: 2500},
+      {name: 'Page G', uv: 3490, amt: 2100},
 ];
 
 export default class HackBarChart extends Component {
@@ -21,9 +21,7 @@ export default class HackBarChart extends Component {
        <YAxis/>
        <CartesianGrid strokeDasharray="3 3"/>
        <Tooltip/>
-       <Legend />
-       <Bar dataKey="pv" fill="#8884d8" />
-       <Bar dataKey="uv" fill="#82ca9d" />
+       <Bar dataKey="uv" fill="#0088FE" />
       </BarChart>
     );
   }
