6d61c730f7a14ab431901cb212951f191fbdaf50
John Games <jgames@amobee.com>
2017-05-23 21:57:38 -0700

    Add style.css locally

--- src/BubbleChart.jsx
+++ src/BubbleChart.jsx
@@ -5,11 +5,11 @@ import './App.css'
 
 var colorLegend = [
   //reds from dark to light
-  {color: "#67000d", text: 'Negative', textColor: "#ffffff"}, "#a50f15", "#cb181d", "#ef3b2c", "#fb6a4a", "#fc9272", "#fcbba1", "#fee0d2",
+  {color: "#67000d", text: 'Small', textColor: "#ffffff"}, "#a50f15", "#cb181d", "#ef3b2c", "#fb6a4a", "#fc9272", "#fcbba1", "#fee0d2",
   //neutral grey
-  {color: "#f0f0f0", text: 'Neutral'},
+  {color: "#f0f0f0", text: 'Medium'},
   // blues from light to dark
-  "#deebf7", "#c6dbef", "#9ecae1", "#6baed6", "#4292c6", "#2171b5", "#08519c", {color: "#08306b", text: 'Positive', textColor: "#ffffff"}
+  "#deebf7", "#c6dbef", "#9ecae1", "#6baed6", "#4292c6", "#2171b5", "#08519c", {color: "#08306b", text: 'Large', textColor: "#ffffff"}
 ];
 
 var tooltipProps = [{
