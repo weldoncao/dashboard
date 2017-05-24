145ecd6f8146e56beb4ffb12efd0c9f3906d0574
John Games <jgames@amobee.com>
2017-05-24 11:51:42 -0700

    hi

--- src/dashboardReducer.js
+++ src/dashboardReducer.js
@@ -7,6 +7,7 @@ import {
 } from './dashboardAction'
 
 import statesData from './data/states-data';
+import axios from 'axios';
 
 const initialStates = {
   sortState: { key: 'regionName', direction: 'ASC' },
@@ -18,7 +19,30 @@ export default function dashboard(state = initialStates, action) {
         case CALL:
             return {...state, fetching: true }
         case CALL_SUCCESS:
-            return {...state, data: action.data, fetching: false }
+         //  var temp;
+         //   var dataWithState = action.data.geo.map(x => {
+         //     if (x.name.length > 5) {
+         //       return;
+	 //     }
+         //     axios.get('https://www.zipcodeapi.com/rest/uWJPxib5467f0kaFKH42zyodvCJoh2KLZtBwLMCPTJ5U3gzOXDQxceLIQu67wQTv/info.json/' + x.name + '/degrees')
+         //       .then(function (response) {
+         //         x['state'] = response.state;
+         //       })
+         //       .catch(function (error) {
+         //         console.log(error);
+         //       });
+         //   })
+
+         //   var groupBy = function(xs, key) {
+         //     return xs.reduce(function(rv, x) {
+         //       rv[x[key]] = rv[x[key]] + x[key];
+         //       return rv;
+         //     }, {});
+         //   };
+
+         //       var reducedData = groupBy(dataWithState, 'state');
+         //   console.log(reducedData)
+            return {...state, data: action.data, fetching: false , geoData: statesData }
         case PULL_DCS:
             return {...state, fetching: true }
         case PULL_DCS_SUCCESS:
